057cc4fea7413005fc1d296f7fd51de7dd78c20d
Mike Howe <mike@mcsnetworks.com>
2014-11-16 20:26:49 -0600

    Changes.
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/almuramc/almuraforgebridge/BridgePlugin.java
+++ src/main/java/com/almuramc/almuraforgebridge/BridgePlugin.java
@@ -1,4 +1,23 @@
 /*
+ * This file is part of Almura Forge Bridge.
+ *
+ * © 2013 AlmuraDev <http://www.almuradev.com/>
+ * Almura Forge Bridge is licensed under the GNU General Public License.
+ *
+ * Almura Forge Bridge is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Almura Forge Bridge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU General Public License.
+ */
+/*
  * This file is part of Almura Control Panel.
  *
  * © 2013 AlmuraDev <http://www.almuradev.com/>
@@ -79,7 +98,12 @@ public class BridgePlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onNickChanged(NickChangeEvent event) {
-        
+        Player player = Bukkit.getPlayer(event.getAffected().getName());
+        System.out.println("Player:" + event.getAffected().getName());
+        if (player == null) {
+            System.out.println("Player is Null");
+        }
+        BridgeNetwork.sendDisplayName(player, event.getValue());
     }
     
     @EventHandler
